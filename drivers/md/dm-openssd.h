f718dfd10fe36984b3e44774e333644584cb6c2e
Matias Bjørling <m@bjorling.me>
2013-07-26 13:56:04 +0200

    openssd: alloc/dealloc of pools, block and aps.

--- drivers/md/dm-openssd.h
+++ drivers/md/dm-openssd.h
@@ -15,4 +15,17 @@
 #define OPENSSD_IOCTL_ID          _IO(OPENSSD_IOC_MAGIC, 0x40)
 #define OPENSSD_IOCTL_SUBMIT_HINT _IOW(OPENSSD_IOC_MAGIC, 0x41, hint_data_t)
 
+#define ssd_for_each_pool(openssd, pool, i)									\
+		for ((i) = 0, pool = &(openssd)->pools[0];							\
+			 (i) < (openssd)->nr_pools; (i)++, pool = &(openssd)->pools[(i)])
+
+#define ssd_for_each_ap(openssd, ap, i)										\
+		for ((i) = 0, ap = &(openssd)->aps[0];								\
+			 (i) < (openssd)->nr_aps; (i)++, ap = &(openssd)->aps[(i)])
+
+#define pool_for_each_block(pool, block, i)									\
+		for ((i) = 0, block = &(pool)->blocks[0];							\
+			 (i) < (pool)->nr_blocks; (i)++, block = &(pool)->blocks[(i)])
+
+
 #endif /* DM_OPENSSD_H_ */

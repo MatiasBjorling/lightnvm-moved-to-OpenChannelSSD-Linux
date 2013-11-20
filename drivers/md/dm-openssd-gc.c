a23ab91b1ffba2ab587b9ee40334acc81b001be8
Matias Bjørling <m@bjorling.me>
2013-11-20 18:28:46 +0100

    openssd: move to event based gc

--- drivers/md/dm-openssd-gc.c
+++ drivers/md/dm-openssd-gc.c
@@ -4,10 +4,19 @@
 /* Run only GC if less than 1/X blocks are free */
 #define GC_LIMIT_INVERSE 10
 
+static void queue_pool_gc(struct nvm_pool *pool)
+{
+	struct openssd *os = pool->os;
+	queue_work(os->kgc_wq, &pool->gc_ws);
+}
+
 void openssd_gc_cb(unsigned long data)
 {
 	struct openssd *os = (void*) data;
-	queue_work(os->kgc_wq, &os->gc_ws);
+	struct nvm_pool *pool;
+	int i;
+//	ssd_for_each_pool(os, pool, i)
+//		queue_pool_gc(pool);
 	mod_timer(&os->gc_timer, jiffies + msecs_to_jiffies(os->config.gc_time));
 }
 
@@ -27,7 +36,7 @@ static struct nvm_block* block_max_invalid(struct nvm_block *a,
 }
 
 /* linearly find the block with highest number of invalid pages 
- * requires pool->lock */
+ * requires pool->gc_lock */
 static struct nvm_block* block_prio_find_max(struct nvm_pool *pool)
 {
 	struct list_head *list = &pool->prio_list;
@@ -61,19 +70,21 @@ static void openssd_move_valid_pages(struct openssd *os, struct nvm_block *block
 	if (bitmap_full(block->invalid_pages, os->nr_host_pages_in_blk))
 		return;
 
+	printk("o1\n");
 	while ((slot = find_next_zero_bit(block->invalid_pages, os->nr_host_pages_in_blk, slot + 1)) < os->nr_host_pages_in_blk) {
 		/* Perform read */
 		p_addr = block_to_addr(block) + slot;
 
 		/* TODO: check for memory failure */
 		src_bio = bio_alloc(GFP_NOIO, 1);
-
 		src_bio->bi_bdev = os->dev->bdev;
 		src_bio->bi_sector = p_addr * NR_PHY_IN_LOG;
+
 		page = mempool_alloc(os->page_pool, GFP_NOIO);
 
 		/* TODO: check return value */
-		bio_add_page(src_bio, page, EXPOSED_PAGE_SIZE, 0);
+		if (!bio_add_page(src_bio, page, EXPOSED_PAGE_SIZE, 0))
+			DMERR("Could not add page");
 
 		openssd_submit_bio(os, block, READ, src_bio, 1);
 
@@ -102,6 +113,7 @@ static void openssd_move_valid_pages(struct openssd *os, struct nvm_block *block
 		mempool_free(page, os->page_pool);
 	}
 	BUG_ON(!bitmap_full(block->invalid_pages, os->nr_host_pages_in_blk));
+	printk("o2\n");
 }
 
 /* Push erase condition to automatically be executed when block goes to zero.
@@ -117,65 +129,62 @@ void openssd_block_release(struct kref *ref)
 
 void openssd_gc_collect(struct work_struct *work)
 {
-	struct openssd *os = container_of(work, struct openssd, gc_ws);
-	struct nvm_pool *pool;
+	struct nvm_pool *pool = container_of(work, struct nvm_pool, gc_ws);
+	struct openssd *os = pool->os;
 	struct nvm_block *block;
 	unsigned int nr_blocks_need;
-	int pid, pid_start;
 
-	block = NULL;
-	/* Iterate the pools once to look for pool that has a block to be freed. */
-	pid_start = os->next_collect_pool % os->nr_pools;
-	for (pid = 0; pid < os->nr_pools; pid++) {
-		pool = &os->pools[(pid + pid_start) % os->nr_pools];
-
-		/* DMDEBUG("pool_id=%d nr_blocks_need %d pool->nr_free_blocks %d", pid, nr_blocks_need, pool->nr_free_blocks); */
-		nr_blocks_need = pool->nr_blocks / 10;
-
-		/*DMINFO("pool_id=%d nr_blocks_need %d pool->nr_free_blocks %d",
-		 * pid, nr_blocks_need, pool->nr_free_blocks);*/
-		//printk("i need %u %u\n", nr_blocks_need, pool->nr_free_blocks);
-		spin_lock(&pool->lock);
-		while (nr_blocks_need > pool->nr_free_blocks) {
-
-			if (list_empty(&pool->prio_list))
-				break;
-
-			block = block_prio_find_max(pool);
-
-			/* this should never happen. Its just here for an extra check */
-			if (!block->nr_invalid_pages)
-				break;
-
-			list_del(&block->prio);
-			spin_unlock(&pool->lock);
-
-			/* this should never happen. Anyway, lets check for it.*/
-			BUG_ON(!block_is_full(block));
-
-			/* take the lock. But also make sure that we haven't messed up the
-			 * gc routine, by removing the global gc lock. */
-			BUG_ON(!atomic_inc_return(&block->gc_running));
-
-			/* rewrite to have moves outside lock. i.e. so we can
-			 * prepare multiple pages in parallel on the attached
-			 * device. */
-			DMDEBUG("moving block addr %ld", block_to_addr(block));
-			openssd_move_valid_pages(os, block);
-
-			kref_put(&block->ref_count, openssd_block_release);
-
-			spin_lock(&pool->lock);
-		}
-		spin_unlock(&pool->lock);
-		//DMERR("Freed %u blocks", i);
+	/* DMDEBUG("pool_id=%d nr_blocks_need %d pool->nr_free_blocks %d", pid, nr_blocks_need, pool->nr_free_blocks); */
+	nr_blocks_need = pool->nr_blocks / 10;
+	/*DMINFO("pool_id=%d nr_blocks_need %d pool->nr_free_blocks %d",
+	 * pid, nr_blocks_need, pool->nr_free_blocks);*/
+	//printk("i need %u %u\n", nr_blocks_need, pool->nr_free_blocks);
+	spin_lock(&pool->gc_lock);
+	if (list_empty(&pool->prio_list)) {
+		spin_unlock(&pool->gc_lock);
+		return;
 	}
 
+	while (nr_blocks_need > pool->nr_free_blocks) {
+		block = block_prio_find_max(pool);
+
+		/* this should never happen. Its just here for an extra check */
+		if (!block->nr_invalid_pages) {
+			printk("o\n");
+			break;
+		}
+
+		list_del(&block->prio);
+		spin_unlock(&pool->gc_lock);
+
+		/* this should never happen. Anyway, lets check for it.*/
+		BUG_ON(!block_is_full(block));
+
+		/* take the lock. But also make sure that we haven't messed up the
+		 * gc routine, by removing the global gc lock. */
+		BUG_ON(atomic_inc_return(&block->gc_running) != 1);
+
+		/* rewrite to have moves outside lock. i.e. so we can
+		 * prepare multiple pages in parallel on the attached
+		 * device. */
+		DMDEBUG("moving block addr %ld", block_to_addr(block));
+		openssd_move_valid_pages(os, block);
+
+		kref_put(&block->ref_count, openssd_block_release);
+
+		spin_lock(&pool->gc_lock);
+	}
+	spin_unlock(&pool->gc_lock);
+	//DMERR("Freed %u blocks", i);
+
 	os->next_collect_pool++;
+	printk("c\n");
+
+	queue_work(os->kbiod_wq, &os->deferred_ws);
 }
 
-void openssd_gc_kick_wait(struct openssd *os)
+void openssd_gc_kick(struct nvm_pool *pool)
 {
-	queue_work(os->kgc_wq, &os->gc_ws);
-	flush_workqueue(os->kgc_wq);
+	BUG_ON(!pool);
+	queue_pool_gc(pool);
 }

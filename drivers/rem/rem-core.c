e7f3d571a3aa0c1d5eaaf8e7551603c3e329ad4e
Matias Bjørling <m@bjorling.me>
2014-03-30 09:16:43 -0700

    rem: add rescan functionality

--- drivers/rem/rem-core.c
+++ drivers/rem/rem-core.c
@@ -1,22 +1,37 @@
 #include <linux/blkdev.h>
 #include <linux/blk-mq.h>
 
-#include <linux/remdev.h>
-#include "remdev.h"
+#include <linux/nvdev.h>
+#include "nvdev.h"
 
-int rem_queue_rq(struct blk_mq_hw_ctx *hctx, struct request *rq)
+int nvd_queue_rq(struct blk_mq_hw_ctx *hctx, struct request *rq)
 {
 	struct rem_map *map = rq->special;
-	return map->ops->remap(hctx, rq);
+	return map->ops->remap_rq(hctx, rq);
 }
+EXPORT_SYMBOL(rem_queue_rq);
 
-struct rem_map *rem_init_rq_remap(struct rem_reg *reg, void *driver_data)
+void nvd_complete_rq(struct request *rq)
+{
+	struct rem_map *map = rq->special;
+	map->ops->complete_rq(struct request *rq);
+}
+EXPORT_SYMBOL(rem_complete_rq);
+
+void nvd_end_rq(struct request *rq, int error)
+{
+	struct nvd_map *map = rq->special;
+	map->ops->end_rq(rq, error);
+}
+EXPORT_SYMBOL(rem_end_rq);
+
+struct nvd_map *rem_init_rq_queue(struct nvd_reg *reg, void *driver_data)
 {
 
 }
-EXPORT_SYMBOL(rem_init_rq_remap);
+EXPORT_SYMBOL(rem_init_rq_queue);
 
-void rem_free_remap(struct rem_map *m)
+void nvd_free_remap(struct nvd_map *m)
 {
 
 }
--- include/linux/remdev.h
+++ include/linux/remdev.h
@@ -1,20 +1,36 @@
-#ifndef REMDEV_H
-#define REMDEV_H
+#ifndef NVDEV_H
+#define NVDEV_H
 
-typedef int (remap_fn)(struct rem_map *map);
+typedef int (remap_fn)(struct nvd_map *map);
+typedef void (end_rq_fn)(struct request *rq);
+typedef int (rescan_nvd_fn)(struct nvd_map *map);
 
-struct rem_ops {
+struct nvd_ops {
 	/*
 	 * Remap request
 	 */
 	remap_fn		*remap;
+
+	/*
+	 * End a remapped request
+	 */
+	end_rq_fn		*end_rq;
+
+	/*
+	 * Complete a remapped request
+	 */
+	complete_rq_fn		*complete_rq;
+
+	/*
+	 * Rescan information from a device.
+	 */
+	rescan_nvd_fn		*rescan_nvd;
 };
 
-struct rem_reg {
-	struct rem_ops		*ops;
+struct nvd_reg {
+	struct nvd_ops		*ops;
 	struct blk_mq_reg	*blk_mq_reg;
-	unsigned int flags;			/* REM_F_* */
+	unsigned int		flags;		/* REM_F_* */
 };
-
 
 #endif

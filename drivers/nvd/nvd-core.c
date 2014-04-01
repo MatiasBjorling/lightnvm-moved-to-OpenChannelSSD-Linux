12adf6661691d0e920ba69a35db2b6a506a97dbb
Matias Bjørling <m@bjorling.me>
2014-04-01 09:30:26 -0700

    nvd: rename functions

--- drivers/nvd/nvd-core.c
+++ drivers/nvd/nvd-core.c
@@ -4,35 +4,75 @@
 #include <linux/nvdev.h>
 #include "nvdev.h"
 
+static LIST_HEAD(_targets);
+static DECLARE_RWSEM(_lock);
+
+inline struct nvd_target_type *nvd_find_target(const char *name)
+{
+	struct nvd_target_type *t;
+
+	list_for_each_entry(t, &_targets, list)
+		if (!strcmp(name, t->name))
+			return t;
+
+	return NULL;
+}
+EXPORT_SYMBOL(nvd_find_target);
+
+int nvd_register_target(struct nvd_target_type *t)
+{
+	int ret = 0;
+
+	down_write(&_lock);
+	if (find_nvd_target_type(t->name))
+		ret = -EEXIST;
+	else
+		list_add(&t->list, &_targets);
+	up_write(&_lock);
+	return ret;
+}
+EXPORT_SYMBOL(nvd_register_target);
+
+void nvd_unregister_target(struct nvd_target_type *t)
+{
+	if (!t)
+		return;
+
+	down_write(&_lock);
+	list_del(&t->list);
+	up_write(&_lock);
+}
+EXPORT_SYMBOL(nvd_unregister_target);
+
 int nvd_queue_rq(struct blk_mq_hw_ctx *hctx, struct request *rq)
 {
-	struct rem_map *map = rq->special;
+	struct nvd_map *map = rq->special;
 	return map->ops->remap_rq(hctx, rq);
 }
-EXPORT_SYMBOL(rem_queue_rq);
+EXPORT_SYMBOL(nvd_queue_rq);
 
 void nvd_complete_rq(struct request *rq)
 {
 	struct rem_map *map = rq->special;
 	map->ops->complete_rq(struct request *rq);
 }
-EXPORT_SYMBOL(rem_complete_rq);
+EXPORT_SYMBOL(nvd_complete_rq);
 
 void nvd_end_rq(struct request *rq, int error)
 {
 	struct nvd_map *map = rq->special;
 	map->ops->end_rq(rq, error);
 }
-EXPORT_SYMBOL(rem_end_rq);
+EXPORT_SYMBOL(nvd_end_rq);
 
-struct nvd_map *rem_init_rq_queue(struct nvd_reg *reg, void *driver_data)
+struct nvd_map *nvd_init_rq_queue(struct nvd_reg *reg, void *driver_data)
 {
-
+	
 }
-EXPORT_SYMBOL(rem_init_rq_queue);
+EXPORT_SYMBOL(nvd_init_rq_queue);
 
 void nvd_free_remap(struct nvd_map *m)
 {
 
 }
-EXPORT_SYMBOL(rem_free_remap);
+EXPORT_SYMBOL(nvd_free_remap);
--- include/linux/nvdev.h
+++ include/linux/nvdev.h
@@ -1,36 +1,61 @@
 #ifndef NVDEV_H
 #define NVDEV_H
 
-typedef int (remap_fn)(struct nvd_map *map);
-typedef void (end_rq_fn)(struct request *rq);
-typedef int (rescan_nvd_fn)(struct nvd_map *map);
+struct nv_queue {
+	struct nvd_target_type *type;
+	
+};
 
-struct nvd_ops {
+typedef int (nvd_remap_fn)(struct nvd_map *map);
+typedef void (nvd_end_rq_fn)(struct request *rq);
+typedef int (nvd_rescan_fn)(struct nvd_map *map);
+typedef int (*nvd_init_fn)(struct nvd_target *);
+typedef void (*nvd_exit_fn)(struct nvd_target *);
+
+struct nvd_target {
+	const char		*name;
+	unsigned int		version[3];
 	/*
 	 * Remap request
 	 */
-	remap_fn		*remap;
+	nvd_remap_fn		*remap;
 
 	/*
 	 * End a remapped request
 	 */
-	end_rq_fn		*end_rq;
+	nvd_end_rq_fn		*end_rq;
 
 	/*
 	 * Complete a remapped request
 	 */
-	complete_rq_fn		*complete_rq;
+	nvd_complete_rq_fn		*complete_rq;
 
 	/*
 	 * Rescan information from a device.
 	 */
-	rescan_nvd_fn		*rescan_nvd;
+	nvd_rescan_fn		*rescan_nvd;
+
+	/*
+	 * Module specific init/teardown
+	 */
+	nvd_init_fn		*init;
+	nvd_exit_fn		*exit;
+
+	/*
+	 * For nvd internal use
+	 */
+	struct list_head	list;
 };
 
 struct nvd_reg {
-	struct nvd_ops		*ops;
+	struct nvd_target	*target;
 	struct blk_mq_reg	*blk_mq_reg;
 	unsigned int		flags;		/* REM_F_* */
 };
+
+/* nvd-core.c */
+int nvd_register_target(struct nvd_target *t);
+void nvd_unregister_target(struct nvd_target *t);
+struct nvd_target *find_nvd_target(const char *name);
 
 #endif

db2805eb797619b0d71bc44c228eec2e9ed82b43
Matias Bjørling <m@bjorling.me>
2014-06-11 12:32:19 +0200

    vsl: more updates

--- include/linux/openvsl.h
+++ include/linux/openvsl.h
@@ -7,15 +7,27 @@ struct openvsl_identify {
 	/* TODO fill in */
 };
 
-typedef struct openvsl_identify (openvsl_identify_fn *)(struct openvsl_dev *dev);
+typedef struct openvsl_identify (openvsl_identify_fn)(struct openvsl_dev *dev);
 
 struct openvsl_dev {
 	struct request_queue *q;
 	struct request_queue *admin_q;
 
-	openvsl_identify_fn identify;
+	openvsl_identify_fn	*identify;
+
+
+	/* Requests */
+	queue_rq_fn		*queue_rq;
+	rq_timed_out_fn		*timeout;
 };
 
+/* OpenVSL configuration */
 int openvsl_rq_data_size();
+int openvsl_config_blk_reg(struct blk_mq_reg *reg);
+struct openvsl_dev *openvsl_alloc();
+void openvsl_free(struct openvsl_dev *dev);
+
+/* Request logic */
+
 
 #endif

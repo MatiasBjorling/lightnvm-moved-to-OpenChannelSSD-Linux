ecbac8bccd74736abef4463ac9abab466df1a8fb
Matias Bjørling <m@bjorling.me>
2014-06-11 11:13:03 +0200

    vsl: updates to vsl

--- include/linux/openvsl.h
+++ include/linux/openvsl.h
@@ -7,13 +7,13 @@ struct openvsl_identify {
 	/* TODO fill in */
 };
 
-typedef struct openvsl_identify_fn *(openvsl_identify)(struct openvsl_dev *dev);
+typedef struct openvsl_identify (openvsl_identify_fn *)(struct openvsl_dev *dev);
 
 struct openvsl_dev {
 	struct request_queue *q;
 	struct request_queue *admin_q;
 
-	openvsl_identify_fn *identify;
+	openvsl_identify_fn identify;
 };
 
 int openvsl_rq_data_size();

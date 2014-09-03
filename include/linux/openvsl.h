171541471cd2294da4b177b0aaaa048e3791d25e
Jesper Madsen <jmad@itu.dk>
2014-09-03 13:47:06 +0200

    vsl: ioctl command renamed for naming consistency

--- include/linux/openvsl.h
+++ include/linux/openvsl.h
@@ -137,15 +137,15 @@ struct vsl_cmd_kv {
 	uint8_t	res[7];
 };
 
-#define VSL_IOC_MAGIC 'O'
-#define VSL_IOCTL_ID 		_IO(VSL_IOC_MAGIC, 0x40)
-#define VSL_IOCTL_CMD_KV	_IOWR(VSL_IOC_MAGIC, 0x50, struct vsl_cmd_kv)
-
 enum VslKvCommands {
 	VSL_KV_GET	= 0x00,
 	VSL_KV_PUT	= 0x01,
 	VSL_KV_UPDATE	= 0x02,
 	VSL_KV_DEL	= 0x04,
 };
+
+#define VSL_IOC_MAGIC 'O'
+#define VSL_IOCTL_ID		_IO(VSL_IOC_MAGIC, 0x40)
+#define VSL_IOCTL_KV		_IOWR(VSL_IOC_MAGIC, 0x50, struct vsl_cmd_kv)
 
 #endif
